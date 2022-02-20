aab8cfad4932ebe1eaba2a794ddb04e619faddf2
sdilipkumar052 <45414462+sdilipkumar052@users.noreply.github.com>
2022-02-20 15:39:45 +0530

    Update CartEntity.java
    comm

--- EcomerceCart-service/src/main/java/com/sanjib/cart/cartEntities/CartEntity.java
+++ EcomerceCart-service/src/main/java/com/sanjib/cart/cartEntities/CartEntity.java
@@ -21,7 +21,6 @@ public class CartEntity {
 	@GeneratedValue
 	private Integer cartId;
 	private Integer userId;
-	private String nativeAdd;
 	
 	@OneToMany(targetEntity = CartDetailEntity.class, cascade = CascadeType.ALL, orphanRemoval = true)
 	@JoinColumn(name = "cartid_fk", referencedColumnName = "cartId")
